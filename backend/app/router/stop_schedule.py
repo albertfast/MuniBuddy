ded323add318087b450259617e4caa8ebbb8ebb2
albertfast <ahmethasimsahiner@icloud.com>
2025-04-04 17:16:29 -0700

    Updated backendfiles

--- backend/app/router/stop_schedule.py
+++ backend/app/router/stop_schedule.py
@@ -1,6 +1,11 @@
 import os
 import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+from app.db.database import SessionLocal
+
+db = SessionLocal()
+bus_service = BusService(db=db)
+
 
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
