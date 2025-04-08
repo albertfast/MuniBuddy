f3bf6938d6c0ae9fc40ac5f0cec52d9d60aba850
albertfast <ahmethasimsahiner@icloud.com>
2025-04-08 07:38:41 -0700

    Updated nearby_bus_router

--- backend/app/router/__init__.py
+++ backend/app/router/__init__.py
@@ -1,5 +1,5 @@
 from . import bus
-from . import nearby_bus_router
+from . import nearby_stops
 from . import stop_schedule
 
-__all__ = ['bus', 'nearby_bus_router', 'stop_schedule']
+__all__ = ['bus', 'nearby_stops', 'stop_schedule']
