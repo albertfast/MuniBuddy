934e0120ba3fea6fbe13eed9e4d799a6211a3168
albertfast <ahmethasimsahiner@icloud.com>
2025-04-11 05:40:27 -0700

    updated backend

--- backend/app/core/singleton.py
+++ backend/app/core/singleton.py
@@ -1,11 +1,11 @@
 # app/core/singleton.py
 from app.services.bus_service import BusService
-from app.services.schedule_service import SchedulerService
+from app.services.schedule_service import get_static_schedule
 
 
 # Global singleton instances
 bus_service = BusService()
-scheduler_service = SchedulerService()
+scheduler_service = get_static_schedule()
 
 # Export the singleton
 __all__ = ["bus_service"]
