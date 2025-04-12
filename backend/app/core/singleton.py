a49e298c31b0d696e2b6b2253a17723e3bd678f3
albertfast <ahmethasimsahiner@icloud.com>
2025-04-12 01:07:34 -0700

    updated backend

--- backend/app/core/singleton.py
+++ backend/app/core/singleton.py
@@ -2,6 +2,6 @@ from app.services.schedule_service import SchedulerService
 from app.services.bus_service import BusService
 
 scheduler_service = SchedulerService()
-bus_service = BusService(scheduler_service)
+bus_service = BusService(scheduler=scheduler_service)
 
 __all__ = ["bus_service", "scheduler_service"]
