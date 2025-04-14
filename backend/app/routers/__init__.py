421f3779bd17b5fae2809357e7f8412a99931fd5
albertfast <ahmethasimsahiner@icloud.com>
2025-04-13 20:52:57 -0700

    updated bart_service

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,5 +1,4 @@
-from . import bus_service
 from . import nearby_stops
 from . import stop_schedule
 
-__all__ = ['bus_service', 'nearby_stops', 'stop_schedule']
+__all__ = ['nearby_stops', 'stop_schedule']
