2e9b31c6244e604f937a7b8e70289103712267c1
albertfast <ahmethasimsahiner@icloud.com>
2025-04-17 13:29:29 -0700

    updated database

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,15 +1,13 @@
-from . import (
-    nearby_stops,
-    stop_schedule,
-    stop_predictions,
-    bart_routes,
-    bus_routes
-)
+from app.routers.bus_routes import router as bus_router
+from app.routers.bart_routes import router as bart_router
+from app.routers.stop_predictions.base import router as stop_predictions_router
+from app.routers.nearby_stops import router as nearby_stops_router
+from app.routers.stop_schedule import router as stop_schedule_router
 
 __all__ = [
-    "nearby_stops",
-    "stop_schedule",
-    "stop_predictions",
-    "bart_routes",
-    "bus_routes"
+    "bus_router",
+    "bart_router",
+    "stop_predictions_router",
+    "nearby_stops_router",
+    "stop_schedule_router",
 ]
