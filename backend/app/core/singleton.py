83c620b023d996c8efd6926a7297bc43e7758ec9
albertfast <ahmethasimsahiner@icloud.com>
2025-04-16 19:46:15 -0700

    updated bart routes

--- backend/app/core/singleton.py
+++ backend/app/core/singleton.py
@@ -1,9 +1,10 @@
-from app.services.schedule_service import SchedulerService
 from app.services.bus_service import BusService
-from app.services.bart_service import bart_service
+from app.services.schedule_service import SchedulerService
+from app.services.bart_service import BartService
 
 
-scheduler_service = SchedulerService()
-bus_service = BusService(scheduler=schedule_service)
+schedule_service = SchedulerService()
+bus_service = BusService(schedule_service)
+bart_service = BartService()
 
 __all__ = ["bus_service", "schedule_service", "bart_service"]
