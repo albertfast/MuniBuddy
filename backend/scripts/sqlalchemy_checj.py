917c10c83d5c4e5cd6b62c125abce0e658c10b02
albertfast <ahmethasimsahiner@icloud.com>
2025-04-17 05:52:31 -0700

    updated database

--- backend/scripts/sqlalchemy_checj.py
+++ backend/scripts/sqlalchemy_checj.py
@@ -1,3 +1,8 @@
+import os
+import sys
+os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.insert(0, os.getcwd())
+
 import pandas as pd
 from sqlalchemy import text
 from app.db.database import engine
