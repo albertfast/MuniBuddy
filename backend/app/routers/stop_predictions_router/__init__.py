a98babb5eb16979fafab2f1cf35fd84b6bc70039
albertfast <ahmethasimsahiner@icloud.com>
2025-04-17 14:53:26 -0700

    updated database

--- backend/app/routers/stop_predictions_router/__init__.py
+++ backend/app/routers/stop_predictions_router/__init__.py
@@ -1,6 +1,6 @@
 from .bus_router import router as bus_router
 from .bart_router import router as bart_router
-from .stop_predictions.base import router as stop_predictions_router
+from .stop_predictions_router.base import router as stop_predictions_router
 from .nearby_stops import router as nearby_stops_router
 from .stop_schedule import router as stop_schedule_router
 
