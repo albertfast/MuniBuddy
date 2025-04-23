680b039763a417b3e918fb80875e5a3d9b598c1a
Ahmet Sahiner <ahmethasimsahiner@icloud.com>
2025-04-23 15:19:16 -0700

    update routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,6 +9,7 @@ from app.routers.bus_router import router as bus_router
 from app.routers.bart_router import router as bart_router
 from app.routers.stop_schedule import router as stop_schedule_router
 from app.routers.nearby_bus_positions import router as bus_positions_router
+from app.routers.combined_stop_router import router as combined_stop_router
 
 load_dotenv()
 
@@ -30,6 +31,7 @@ app.include_router(bus_router, prefix="/api/v1")
 app.include_router(bart_router, prefix="/api/v1")
 app.include_router(stop_schedule_router, prefix="/api/v1")
 app.include_router(bus_positions_router, prefix="/api/v1")
+app.include_router(combined_stop_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
