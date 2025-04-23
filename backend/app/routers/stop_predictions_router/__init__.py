0ddb4a79ab6ee7136c23c12a183da12ea5942711
Ahmet Sahiner <ahmethasimsahiner@icloud.com>
2025-04-23 12:27:47 -0700

    update bart_service

--- backend/app/routers/stop_predictions_router/__init__.py
+++ backend/app/routers/stop_predictions_router/__init__.py
@@ -1,4 +1,3 @@
-from .base import router as stop_predictions_router
 from ..bus_router import router as bus_router
 from ..bart_router import router as bart_router
 from ..nearby_stops import router as nearby_stops_router
@@ -8,7 +7,6 @@ from ..stop_schedule import router as stop_schedule_router
 __all__ = [
     "bus_router",
     "bart_router",
-    "stop_predictions_router",
     "nearby_stops_router",
     "stop_schedule_router",
     "bus_positions_router",
