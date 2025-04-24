bd9d774f1c41f36b1e53f5b4cb3c2f097d925a6f
Ahmet Sahiner <ahmethasimsahiner@icloud.com>
2025-04-23 19:41:32 -0700

    update fastapi

--- backend/app/models/bus_route.py
+++ backend/app/models/bus_route.py
@@ -1,18 +1,18 @@
-from sqlalchemy import Column, String, Integer
-from app.db.database import Base
+# from sqlalchemy import Column, String, Integer
+# from app.db.database import Base
 
-class BusRoute(Base):
-    __tablename__ = "bus_routes"
-    __table_args__ = {"extend_existing": True}
+# class BusRoute(Base):
+#     __tablename__ = "bus_routes"
+#     __table_args__ = {"extend_existing": True}
 
-    id = Column(Integer, primary_key=True)
-    route_id = Column(String, unique=True)
-    line_ref = Column(String)
-    agency_id = Column(String)
-    route_name = Column(String)
-    direction = Column(String)
-    origin = Column(String)
-    destination = Column(String)
+#     id = Column(Integer, primary_key=True)
+#     route_id = Column(String, unique=True)
+#     line_ref = Column(String)
+#     agency_id = Column(String)
+#     route_name = Column(String)
+#     direction = Column(String)
+#     origin = Column(String)
+#     destination = Column(String)
 
-    def __repr__(self):
-        return f"<BusRoute {self.route_name} ({self.agency_id})>"
+#     def __repr__(self):
+#         return f"<BusRoute {self.route_name} ({self.agency_id})>"
