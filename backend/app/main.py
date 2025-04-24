345b9e6e002da5add5b45079ecf868c702e34153
Ahmet Sahiner <ahmethasimsahiner@icloud.com>
2025-04-23 18:11:50 -0700

    update fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ from app.routers.nearby_stops import router as nearby_stops_router
 from app.routers.bus_router import router as bus_router
 from app.routers.bart_router import router as bart_router
 from app.routers.stop_schedule import router as stop_schedule_router
-from app.routers.nearby_bus_positions import router as bus_positions_router
+# from app.routers.nearby_bus_positions import router as bus_positions_router
 
 load_dotenv()
 
@@ -31,7 +31,7 @@ app.include_router(nearby_stops_router, prefix="/api/v1")
 app.include_router(bus_router, prefix="/api/v1")
 app.include_router(bart_router, prefix="/api/v1")
 app.include_router(stop_schedule_router, prefix="/api/v1")
-app.include_router(bus_positions_router, prefix="/api/v1")
+# app.include_router(bus_positions_router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
